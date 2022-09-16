39e8547b9a375bb342dfcd168d4e9ec22927bee1
BernhardRode <mail@bernhardrode.de>
2022-09-16 09:49:42 +0000

    Deploying to gh-pages from @ BernhardRode/rode-catering-de@425bc7d589405ad1360e7e243b239d63d1d236ba 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-59a34b66ad1d7c4b4b3f.js"
+    "url": "webpack-runtime-95742a51ae143075c06d.js"
   },
   {
     "url": "framework-e17bf2cd30f16af3f585.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "36afd3ec79ba3a6ab1d27066b545450b"
+    "revision": "f687600d00f671bfadac0890293e7100"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9e0d1e804f1f6a7f6f73.js"
